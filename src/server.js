f3678de0312859e887ad04c89b5d389a5c5a7e6f
Vinícius Oliveira <vinitag190@gmail.com>
2020-10-16 10:58:14 -0300

    Workshop | Dia 15/10 (pausa 2)

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-console.log('Funciona, Bitch')
+//   importando dependência 
+const express = require('express');
+const path = require('path')
+
+//  iniciando o express
+const server = express()
+
+server
+//  utilizando os arquivos estátticos
+.use(express.static('public'))
+
+//  criando rota
+.get('/', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+//  ligando o servidor
+server.listen(5500)
