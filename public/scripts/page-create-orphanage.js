3600e0ff1b752f503e64c3b5699e74b235282c0f
Vinícius Oliveira <vinitag190@gmail.com>
2020-10-15 22:50:48 -0300

    Revert "Revert "Workshop | Dia 14/10 (finalização)""
    This reverts commit 45b7270645690a433bff48febfaf7cd3ddb97780.

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -70,3 +70,19 @@ function deleteField(event) {
   //deletar o campo
   span.parentNode.remove();
 }
+
+function toggleSelect(event) {
+  //retirar a class .active (dos botoes)
+  document.querySelectorAll(".button-select button").forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  //colocar a class .active no botao clicado
+  const button = event.currentTarget;
+  button.classList.add("active");
+
+  //atualizar o input hidden com o valor selecionado
+  const input = document.querySelector('[name = "open_on_weekends"]');
+
+  input.value = button.dataset.value;
+}
